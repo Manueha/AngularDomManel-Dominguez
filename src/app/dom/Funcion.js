a3cc98ca61f016722a35679615a55b6301b597e8
jordel127 <joel.perez@institutvidreres.cat>
2023-12-02 14:17:41 +0100

    Exercici9

--- src/app/dom/Funcion.js
+++ src/app/dom/Funcion.js
@@ -14,7 +14,12 @@ export function  countWord(){
   }
   return comptador;
 }
-export function imagen (){
-  let iemg = document.getElementById("imatge").innerHTML
-  ieme.innerHTML = <img src="https://static.nationalgeographicla.com/files/styles/image_3200/public/suriname-toad.jpg?w=710&h=472"></img>
+export function imagen() {
+
+}
+
+
+export function mostrarValors(){
+  let nodeTitol = document.getElementById("resultats")
+  nodeTitol.innerHTML = document.getElementById("llistaDesplegable").value
 }
